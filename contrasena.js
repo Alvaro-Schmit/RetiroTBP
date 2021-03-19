e8315c9d1a9de54693493077424b81c0705e6c5c
Alvaro Schmit <alvaroschmit@gmail.com>
2021-03-18 22:36:57 -0300

    inicio git

---
+++ contrasena.js
@@ -0,0 +1,29 @@
+function ir(){
+let c=108;
+if(document.form.password.value==c){
+
+window.location="index.html";}
+else{
+alert('Contrasena incorreta');
+}
+}
+
+/*//////////////////////////////////////////*/
+
+
+function desplazarMe(titulo) {
+  $('html, body').animate({
+      scrollTop: $('#' + titulo).offset().top
+  }, 1600)
+}
+
+
+
+$('#llenarPlantilla').click(function() {
+  desplazarMe("plantilla")
+})
+
+
+$('#btnVolverACotizar').click(function() {
+  desplazarMe("plantilla")
+})
---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+function desplazarMe(titulo) {
+    $('html, body').animate({
+        scrollTop: $('#' + titulo).offset().top
+    }, 1600)
+}
+
+
+
+$('#btn_yoga').click(function() {
+    desplazarMe("plantilla")
+})
+
